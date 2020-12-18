d7422d43b34617008d81b9dce5b1327f5c8d3093
nokihara <nokihara-y2896@keio.jp>
2020-12-18 16:48:18 +0900

    originalfile

--- main.py
+++ main.py
@@ -8,18 +8,17 @@ import torchvision.datasets as dset
 import torchvision.transforms as transforms
 import torchvision.utils as vutils
 import matplotlib.pyplot as plt  # グラフ作成用ライブラリー
-from PIL import Image, ImageFilter
 
 # Initial setting
 workers = 2
-batch_size = 64
+batch_size = 49
 nz = 100
 nch_g = 64
 nch_d = 64
-n_epoch = 50  
+n_epoch = 50
 lr = 0.0002
 beta1 = 0.5
-outf = './All50e_result_cgan'
+outf = './result_cgan'
 display_interval = 100
 plt.rcParams['figure.figsize'] = 10, 6  # グラフの大きさ指定
 
@@ -113,17 +112,17 @@ class Discriminator(nn.Module):
             x = layer(x)
         return x.squeeze()     
 
-def onehot_encode(label, device, n_class=40):  
+def onehot_encode(label, device, n_class=7):  
     # ラベルをOne-Hoe形式に変換
     eye = torch.eye(n_class, device=device)
     # ランダムベクトルあるいは画像と連結するために(B, c_class, 1, 1)のTensorにして戻す
     return eye[label].view(-1, n_class, 1, 1)   
 
-def concat_image_label(image, label, device, n_class=40):
+def concat_image_label(image, label, device, n_class=7):
     # 画像とラベルを連結する
     B, C, H, W = image.shape    # 画像Tensorの大きさを取得
-    label = label.reshape((B, n_class, 1,1))
-    label = oh_label.expand(B, n_class, H, W)  # ラベルを画像サイズに拡大
+    oh_label = onehot_encode(label, device)       # ラベルをOne-Hot形式に変換
+    oh_label = oh_label.expand(B, n_class, H, W)  # ラベルを画像サイズに拡大
     return torch.cat((image, oh_label), dim=1)    # 画像とラベルをチャネル方向（dim=1）で連結
 
 def concat_noise_label(noise, label, device):
@@ -131,57 +130,18 @@ def concat_noise_label(noise, label, device):
     oh_label = onehot_encode(label, device)     # ラベルをOne-Hot形式に変換
     return torch.cat((noise, oh_label), dim=1)  # ランダムベクトルとラベルを連結
 
-
-class Mydatasets(torch.utils.data.Dataset):
-        def __init__(self, dir_path, transform = None):
-            self.transform = transform
-            self.data = []
-            self.dir_path = dir_path
-            self.attr = []
-            attr_list =[]
-            with open("list_attr_celeba.csv","r") as f:
-                for i in range(202599):
-                    line1 = f.readline()
-                    line = line1.split(",")
-                    label = []
-                    for j in range(1,41):
-                        if line[j] == "1":
-                            k = 1
-                        if line[j] == "-1":
-                            k = 0
-                        label.append(k)
-                    attr_list.append(label)
-                    self.data.append(line[0])
-                    #break
-            attr_list = np.asarray(attr_list)
-            self.attr= torch.from_numpy(attr_list)
-            self.datanum = len(self.attr)
-
-        def __len__(self):
-            return self.datanum
-
-        def __getitem__(self, idx):
-            out_data = Image.open(self.dir_path + self.data[idx])
-            out_attr = self.attr[idx]
-            if self.transform:
-                out_data = self.transform(out_data)
-            return out_data, out_attr
         
 
 
 def main():
-    transform=transforms.Compose([
-            transforms.CenterCrop(160),
-            transforms.Resize((128, 128)),
-            transforms.RandomResizedCrop(64, scale=(0.9, 1.0), ratio=(1., 1.)),
-            transforms.RandomHorizontalFlip(),
-            transforms.ColorJitter(brightness=0.05, contrast=0.05, saturation=0.05, hue=0.05),
-            transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
-                             ])
-    dir_path = "/home/nokihara/path/to/celeba/img_align_celeba/img_align_celeba/"
-    
-    dataset = Mydatasets(dir_path, transform)
+    dataset = dset.ImageFolder(root='~/path/to/celeba/img_align_celeba',
+                               transform=transforms.Compose([
+                                   transforms.RandomResizedCrop(64, scale=(0.9, 1.0), ratio=(1., 1.)),
+                                   transforms.RandomHorizontalFlip(),
+                                   transforms.ColorJitter(brightness=0.05, contrast=0.05, saturation=0.05, hue=0.05),
+                                   transforms.ToTensor(),
+                                   transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                               ]))
 
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                          shuffle=True, num_workers=int(workers))
@@ -189,11 +149,11 @@ def main():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     print('device:', device)
 
-    netG = Generator(nz=nz+40, nch_g=nch_g).to(device)   # 入力ベクトルの次元は、nz+クラス数7
+    netG = Generator(nz=nz+7, nch_g=nch_g).to(device)   # 入力ベクトルの次元は、nz+クラス数7
     netG.apply(weights_init)
     print(netG)
 
-    netD = Discriminator(nch=3+40, nch_d=nch_d).to(device)   # 入力Tensorのチャネル数は、nch+クラス数7
+    netD = Discriminator(nch=3+7, nch_d=nch_d).to(device)   # 入力Tensorのチャネル数は、nch+クラス数7
     netD.apply(weights_init)
     print(netD)
 
@@ -202,13 +162,14 @@ def main():
     optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999), weight_decay=1e-5)
     optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999), weight_decay=1e-5)
 
-    fixed_noise = torch.randn(batch_size, nz, 1, 1, device=device)
+    fixed_noise = torch.randn(40, nz, 1, 1, device=device)
 
-    fixed_label = [i for i in range(40)] * (batch_size // 40)  # 0〜6のラベルの繰り返し
+    fixed_label = [i for i in range(7)] * (batch_size // 7)  # 0〜6のラベルの繰り返し
     fixed_label = torch.tensor(fixed_label, dtype=torch.long, device=device)
 
-    fixed_noise_label = concat_noise_label(fixed_noise, fixed_label, device)  
+    fixed_noise_label = concat_noise_label(fixed_noise, fixed_label, device)
     Loss_D_list, Loss_G_list = [], []  # グラフ作成用リスト初期化
+
 
     # traning loop
     for epoch in range(n_epoch):
@@ -219,7 +180,7 @@ def main():
 
             sample_size = real_image.size(0)  # 画像枚数
             noise = torch.randn(sample_size, nz, 1, 1, device=device)  # ランダムベクトル生成（ノイズ）
-            fake_label = torch.randint(40, (sample_size,), dtype=torch.long, device=device)  # 偽物画像のラベル
+            fake_label = torch.randint(7, (sample_size,), dtype=torch.long, device=device)  # 偽物画像のラベル
             fake_noise_label = concat_noise_label(noise, fake_label, device)  # ランダムベクトルとラベルを連結
         
             real_target = torch.full((sample_size,), 1., device=device)  
@@ -232,7 +193,7 @@ def main():
             errD_real = criterion(output, real_target) 
             D_x = output.mean().item()  
 
-            fake_image = netG(fake_noise_label)  # Generatorが生成した偽物画像
+            fake_image = netG(fake_noise_label)  # Generatorが生成した偽物画像 
             fake_image_label = concat_image_label(fake_image, fake_label, device)   # 偽物画像とラベルを連結
 
             output = netD(fake_image_label.detach())  
@@ -258,9 +219,10 @@ def main():
                       .format(epoch + 1, n_epoch,
                               itr + 1, len(dataloader),
                               errD.item(), errG.item(), D_x, D_G_z1, D_G_z2))
-            
+
                 Loss_D_list.append(errD.item())  # Loss_Dデータ蓄積 (グラフ用)
                 Loss_G_list.append(errG.item())  # Loss_Gデータ蓄積 (グラフ用)
+
             
             if epoch == 0 and itr == 0:
                 vutils.save_image(real_image, '{}/real_samples.png'.format(outf),
@@ -277,7 +239,7 @@ def main():
             torch.save(netD.state_dict(), '{}/netD_epoch_{}.pth'.format(outf, epoch + 1))
 
     # グラフ作成
-    plt.figure()    
+    plt.figure()
     plt.plot(range(len(Loss_D_list)), Loss_D_list, color='blue', linestyle='-', label='Loss_D')
     plt.plot(range(len(Loss_G_list)), Loss_G_list, color='red', linestyle='-', label='Loss_G')
     plt.legend()
@@ -285,7 +247,7 @@ def main():
     plt.ylabel('loss')
     plt.title('Loss_D and Loss_G')
     plt.grid()
-    plt.savefig('All50e_Loss_graph.png')          
+    plt.savefig('Loss_graph.png') 
 
 if __name__ == '__main__':
     main()    
